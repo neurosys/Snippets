640d5339b07016af802d54282fa0ceb3d66858f1
neurosys <blabla>
2016-02-06 07:07:00 +0200

    Added the main environment for the tests

--- cpp/hexdump_tests.cpp
+++ cpp/hexdump_tests.cpp
@@ -1,4 +1,12 @@
+#include <stdio.h>
+
 #include "hexdump.h"
 #include "hexdump_tests.h"
 
 
+bool hexdump_tests_main(bool verbose)
+{
+    bool rez = false;
+    printf("hexdump_tests_main() = %s\n", (rez) ? "[Passed]" : "[FAILED]"); 
+    return false;
+}
