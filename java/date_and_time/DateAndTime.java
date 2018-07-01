46a2d86412d2578124caaf433aea1ea536f4ffc6
NeuroSys <you@example.com>
2018-07-01 23:30:32 +0300

    Show and parse date and timestamps

---
+++ java/date_and_time/DateAndTime.java
@@ -0,0 +1,41 @@
+import java.text.DateFormat;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+public class DateAndTime {
+	
+	public static void printDateAndTime() {
+		LocalDate localDate = LocalDate.now();
+		int dayofYear = localDate.getDayOfYear();
+		int dayOfMonth = localDate.getDayOfMonth();
+		int dayOfWeek = localDate.getDayOfWeek().getValue(); 
+		int month = localDate.getMonthValue();
+		int year = localDate.getYear();
+		
+		LocalDateTime localDateTime = LocalDateTime.now();
+		int hour = localDateTime.getHour();
+		int minutes = localDateTime.getMinute();
+		int seconds = localDateTime.getSecond();
+		int nanoseconds = localDateTime.getNano();
+		
+		System.out.println(localDate.toString());
+		System.out.println(localDateTime.toString());
+		System.out.println(LocalTime.now().toString());
+		System.out.println("unix timestamp = (ms) " + Instant.now().toEpochMilli());
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_kk-mm-ss:SSS");
+		System.out.println("Printed with my format: " + localDateTime.format(formatter));
+	}
+
+    public static void parseTimestamp() {
+		String myTimestamp = "2018-06-30_11-30-24:443";
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_kk-mm-ss:SSS");
+		LocalDateTime ldt = LocalDateTime.parse(myTimestamp, formatter);
+		System.out.println("Parsed timestamp = " + ldt.toString());
+    }
+	public static void main(String[] args) {
+		printDateAndTime();
+        parseTimestamp();
+	}
+
+}
