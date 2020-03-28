3121581a3718beec7585db5a4c6a438ad62b223d
Catalin Amza <camza@luxoft.com>
2020-03-28 12:17:14 +0200

    Java How to get the login name of the current user

---
+++ java/misc/LoginUser.java
@@ -0,0 +1,7 @@
+
+public class LoginUser {
+    public static void main(String[] argv) {
+        String nameOfUserUsedForLogin = System.getProperty("user.name");
+        System.out.println(nameOfUserUsedForLogin);
+    }
+}
